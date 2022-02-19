5f2fd5219a296a52d2d4e78cdf6a31a04fc9ee14
Matias1428 <82426187+Matias1428@users.noreply.github.com>
2022-02-18 18:46:11 -0600

    creating a user interface with python tkinter library

---
+++ User Interface.py
@@ -0,0 +1,12 @@
+from tkinter import*
+#Create a window
+root=Tk()
+
+#Creating a label widget
+myLabel=Label(root, text="Hello world")
+
+#Shoving it onto the screen
+myLabel.pack()
+
+#Loop
+root.mainloop()
