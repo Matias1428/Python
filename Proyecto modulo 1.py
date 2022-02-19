64ec1549f49468b1d652e4d1428d563bae03a605
Matias1428 <82426187+Matias1428@users.noreply.github.com>
2022-02-19 15:37:26 -0600

    print lento

--- Proyecto modulo 1.py
+++ Proyecto modulo 1.py
@@ -14,24 +14,27 @@ class Pokemon:
     self.current_stats = self.stats
 
   def pelea(self, rival):
-    
-    # Tu rival es furte o debil a ti?
+    # Imprimir información acerca de la pelea
+    printRetardado("-----Pelea pokemon-----\n")
+    # Tu rival es furte o debil a ti? 
     if self.tipo in rival.fortalezas:
       modificador_ataque = 1/2
-      print(f"{rival.especie} es fuerte a los ataques de {self.especie} \n")
+      printRetardado({rival.especie} +" es fuerte a los ataques de "+ {self.especie}+ "\n")
     elif self.tipo in rival.debilidades:
       modificador_ataque = 2
-      print(f"{rival.especie} es debil a los ataques de {self.especie} \n")
+      printRetardado(f"{rival.especie} es debil a los ataques de {self.especie} \n")
     else:
       modificador_ataque = 1
+
+
     
     #eres fuerte o debil a tu rival?
     if rival.tipo in self.fortalezas:
       modificador_defensa = 1/2
-      print(f"{self.especie} es fuerte a los ataques de {rival.especie} \n")
+      printRetardado(f"{self.especie} es fuerte a los ataques de {rival.especie} \n")
     elif rival.tipo in self.debilidades:
       modificador_defensa = 2
-      print(f"{self.especie} es debil a los ataques de {rival.especie} \n")
+      printRetardado(f"{self.especie} es debil a los ataques de {rival.especie} \n")
     else:
       modificador_defensa = 1
 
@@ -46,16 +49,18 @@ class Pokemon:
       if mi_turno:
         ataque_elegido=ataque_elegir(self);
         #print("¿Que movimiento desea utilizar?\n")
-        print(self.especie + "_utiliza ", ataque_elegido[0])
+        modificador_ataque2=modificadorAtaque(ataque_elegido,self,rival)
+        printRetardado(self.especie + " utiliza " + ataque_elegido[0]+"\n")
+        
 
         # atacando
         dano = int(
             self.dano_base * 
             (ataque_elegido[1] / rival.current_stats["defensa"]) * 
-            modificador_ataque) 
+            modificador_ataque*modificador_ataque2) 
         rival.current_stats["hp"] -= dano
-        print(f"{self.especie} hizo {dano} de daño a {rival.especie}")
-        print(f"A {rival.especie} le quedan {rival.current_stats['hp']} puntos de vida")
+        printRetardado(self.especie +" hizo "+ str(dano)+" de daño a "+rival.especie+"\n")
+        printRetardado("A "+ rival.especie+" le quedan "+ str(rival.current_stats['hp'])+" puntos de vida\n")
       else:
         # defendiendo
         dano = int(
@@ -63,14 +68,14 @@ class Pokemon:
             (rival.current_stats["ataque"] / self.current_stats["defensa"]) * 
             modificador_defensa)
         self.current_stats["hp"] -= dano
-        print(f"{rival.especie} hizo {dano} de daño a {self.especie}")
-        print(f"A {self.especie} le quedan {self.current_stats['hp']} puntos de vida")
+        printRetardado(rival.especie+ " hizo "+ str(dano)+ " de daño a "+self.especie+"\n")
+        printRetardado("A "+self.especie+" le quedan "+str(self.current_stats['hp'])+ " puntos de vida\n")
       mi_turno = not mi_turno
     else:
       if self.current_stats["hp"] <= 0:
-        print(f'{rival.especie} ha ganado la pelea \n')
+        printRetardado(rival.especie+ " ha ganado la pelea \n")
       else:
-        print(f'{self.especie} ha ganado la pelea \n')
+        printRetardado(self.especie +' ha ganado la pelea \n')
 
 squirtle = Pokemon(
     especie = "Squirtle",
@@ -117,27 +122,26 @@ bulbasaur = Pokemon(
                "3":['Calcinación', 29,'Fuego'],
                "4":['Ascuas', 10,'Fuego']})
 
-
 def pokemon_Elegir(pokemon_elegido):
   if pokemon_elegido=='squirtle' or pokemon_elegido=='1':
-      print('Has elegido a  squirtle\n')
+      printRetardado('Has elegido a  squirtle\n')
       squirtle.pelea(charmander)
       squirtle.centro_pokemon()
       charmander.centro_pokemon()
 
-  if pokemon_elegido=='charmander'  or pokemon_elegido=='2':
-      print('Has elegido a charmander\n')
+  elif pokemon_elegido=='charmander'  or pokemon_elegido=='2':
+      printRetardado('Has elegido a charmander\n')
       charmander.pelea(bulbasaur)
       bulbasaur.centro_pokemon()
       charmander.centro_pokemon()
 
-  if pokemon_elegido=='bulbasaur'  or pokemon_elegido=='3':
-      print('Has elegido a bulbasaur\n')
+  elif pokemon_elegido=='bulbasaur'  or pokemon_elegido=='3':
+      printRetardado('Has elegido a bulbasaur\n')
       bulbasaur.pelea(squirtle)
       bulbasaur.centro_pokemon()
       squirtle.centro_pokemon()
-  #if pokemon_elegido!='bulbasaur'  or pokemon_elegido!='3' or pokemon_elegido!='charmander'  or pokemon_elegido!='2' or pokemon_elegido!='squirtle' or pokemon_elegido!='1':
-    # print('Pokemon no disponible')
+  else:
+      printRetardado('Pokemon no disponible')
     # print("1.-squirtle\n2.-charmander \n3.-bulbasaur")
      #pokemon_elegido= input()
      #pokemon_Elegir(pokemon_elegido)
@@ -149,20 +153,57 @@ def ataque_elegir(self):
   if ataque_recibido=="1":    
     ataque_elegido=self.ataques['1']
     
-  if ataque_recibido=="2":
+  elif ataque_recibido=="2":
     ataque_elegido=self.ataques['2']
 
-  if ataque_recibido=="3":
+  elif ataque_recibido=="3":
     ataque_elegido=self.ataques['3']
 
-  if ataque_recibido=="4":
+  elif ataque_recibido=="4":
     ataque_elegido=self.ataques['4']
-    
+  else :
+    cadena=('Error ataque elegido') 
+    for c in cadena:
+      print(c, end='')
+      sys.stdout.flush()
+      sleep(0.1) 
   return ataque_elegido
 
+#Fortalezas y debilidades en ataques
+def modificadorAtaque(self, pokemonCompleto,rival):
+    # Imprimir información acerca de la pelea
+    print("-----Modificador de ataque pokemon-----")
+    # Tu rival es furte o debil a ti? 
+    if self[2] in rival.fortalezas:
+      modificador_ataque = 1/2
+      print(f"{rival.especie} es fuerte a los ataques de {pokemonCompleto.especie} \n")
+    elif self[2] in rival.debilidades:
+      modificador_ataque = 2
+      print(f"{rival.especie} es debil a los ataques de {pokemonCompleto.especie} \n")
+    else:
+      modificador_ataque = 1
+      cadena='no esta\n'
+      for c in cadena:
+        print(c, end='')
+        sys.stdout.flush()
+        sleep(0.1)
+
+
+    return modificador_ataque
+
+def printRetardado(cadena):
+  for c in cadena:
+    print(c, end='')
+    sys.stdout.flush()
+    sleep(0.1)
+
+from time import sleep
+import sys
 print("Te han desafiado a una pelea \n")
-print("¿Que pokemon desea utilizar? \n")
+printRetardado("¿Que pokemon desea utilizar? \n")
+
 print("1.-squirtle\n2.-charmander \n3.-bulbasaur")
+
 pokemon_elegido= input()
 pokemon_Elegir(pokemon_elegido)
 #squirtle.pelea(charmander)
