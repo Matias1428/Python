c7cb3c518666357e25f64d5a9de6cdc296a63abd
Matias1428 <82426187+Matias1428@users.noreply.github.com>
2022-02-20 18:52:04 -0600

    Update P1.py

--- P1.py
+++ P1.py
@@ -2,17 +2,32 @@ from traceback import print_exception
 
 
 class Item:
-    def __init__(self,name,price,quantity):
+    pay_rate= 0.8 # The pay rate after 20% discount
+    def __init__(self,name: str,price: float,quantity=0):
+        #Run validations to the received arguments
+        assert price >= 0, f"Price {price} is not greater then zero"
+        assert quantity >= 0, f"Quantity {quantity} is not greater then zero"
+
+        
         print("I am created!")
+        #Assign to self object
         self.name=name
         self.price=price
         self.quantity=quantity
 
-    def calculate_total_price(self,x,y):
-        return x*y
+    def calculate_total_price(self):
+        return self.price*self.quantity
 
-item1= Item("Phone",110,5)
+    def apply_discount(self):
+        self.price=self.price*Item.pay_rate 
+
+#item1= Item("Phone",110)
+item1= Item("Phone",1)
 item2= Item("Pc",200,4)
 
-print(item1.name)
+item2.has_numpad=False;
 
+print(item2.calculate_total_price())  
+print(Item.pay_rate)
+print(Item.__dict__)
+print(item1.__dict__)
