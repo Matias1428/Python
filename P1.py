c25c53fb8ba0f0b825362c7628aa3ee91e65f0ad
Matias1428 <82426187+Matias1428@users.noreply.github.com>
2022-02-20 18:26:00 -0600

    object oriented with python

--- P1.py
+++ P1.py
@@ -1,1 +1,18 @@
-print('hello')
+from traceback import print_exception
+
+
+class Item:
+    def __init__(self,name,price,quantity):
+        print("I am created!")
+        self.name=name
+        self.price=price
+        self.quantity=quantity
+
+    def calculate_total_price(self,x,y):
+        return x*y
+
+item1= Item("Phone",110,5)
+item2= Item("Pc",200,4)
+
+print(item1.name)
+
