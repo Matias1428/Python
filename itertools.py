a3d4fd9915c4d41bec28efb59347cecfc4dad2f6
Matias1428 <82426187+Matias1428@users.noreply.github.com>
2022-02-25 18:32:37 -0600

    Create itertools.py

---
+++ itertools.py
@@ -0,0 +1,7 @@
+#itertools: product, permutations, combinations, accumulate, groupby, and infinite iterartors
+from itertools import product
+from re import A
+a=[1,2]
+b=[3,4]
+prod=product(a,b)
+print(list(prod))
