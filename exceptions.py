be77e82b4ebbbf8232ed1142ba7d5e3b5f0f71f7
Matias1428 <82426187+Matias1428@users.noreply.github.com>
2022-02-27 19:54:04 -0600

    Update exceptions.py

--- exceptions.py
+++ exceptions.py
@@ -1,3 +1,50 @@
 # Errors and Exceptions
-x=-5
-assert(x>=0), 'x is no positive'
+from email import message
+from warnings import catch_warnings
+
+
+#x=-5
+#assert(x>=0), 'x is no positive'
+
+try:
+    a=5/0
+except:
+    print('error happed')
+
+
+try:
+    a=7/0
+except Exception as e:
+    print(e)
+
+try:
+    a=7/0
+    b=a+'10'
+except ZeroDivisionError as e:
+    print(e)
+except TypeError as e:
+    print(e)
+else:
+    print('everything is fine')
+finally:  # alwasy run
+    print('Cleaning up...')
+
+
+class ValueTooHighError(Exception):
+    pass
+class ValueTooSmallError(Exception):
+    def __init__(self,message,value):
+        self.message=message
+        self.value=value
+
+def test_value(x):
+    if x>100:
+        raise ValueTooHighError('Values is too high')
+    if x<5:
+        raise ValueTooSmallError('values is too small', x)
+try:
+    test_value(1)    
+except ValueTooHighError as e:
+    print(e)
+except ValueTooSmallError as e:
+    print(e.message, e.value)
