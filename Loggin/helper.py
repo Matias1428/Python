ae8b0312a40e01aec90442cd28a164473bb9bf1d
Matias1428 <82426187+Matias1428@users.noreply.github.com>
2022-02-27 20:26:50 -0600

    main logger

--- Loggin/helper.py
+++ Loggin/helper.py
@@ -1,8 +1,12 @@
+from asyncio.log import logger
 import logging
-logging.basicConfig(level=logging.DEBUG,format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',  cldatefmt='%m/%d/%Y %H:%M:%S')
+logger=logging.getLogger(__name__)
+logger.info('hello from helper')
 
-logging.debug('this is a bug message')
-logging.info('this is an info message')
-logging.warning('this is a warning message')
-logging.error('this is an error message')
-logging.critical('this is a critical message')
+#logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',  datefmt='%m/%d/%Y %H:%M:%S')
+
+#logging.debug('this is a bug message')
+#logging.info('this is an info message')
+#logging.warning('this is a warning message')
+#logging.error('this is an error message')
+#logging.critical('this is a critical message')
